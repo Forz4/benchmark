407fb53bd836448a71ba368f5c70de5045dd8485
Forz4 <306500408@qq.com>
2019-07-08 09:40:17 +0800

    add timersub

--- benchmark.h
+++ benchmark.h
@@ -46,4 +46,15 @@ char level_print[7][10] = { "LOGNON" , \
                             "LOGINF" , \
                             "LOGADT" , \
                             "LOGDBG" };
+
+#define	timersub(tvp, uvp, vvp)						        \
+	do {								                    \
+		(vvp)->tv_sec = (tvp)->tv_sec - (uvp)->tv_sec;		\
+		(vvp)->tv_usec = (tvp)->tv_usec - (uvp)->tv_usec;	\
+		if ((vvp)->tv_usec < 0) {				            \
+			(vvp)->tv_sec--;				                \
+			(vvp)->tv_usec += 1000000;			            \
+		}							                        \
+	} while (0)
+
 #endif
